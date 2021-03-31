52900c3bb2c5af12d1a9d200db2b35ac6eb77b00
rkdud007 <gayeongparkk@gmail.com>
2021-03-31 16:13:13 +0900

    first

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "박가영"
+  const age = 21
+  const height = 169
+  const major = "건축사회환경공학부"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>제이름은 {name}입니다.</h1>
+      <h2>제 나이는 {age} 입니다.</h2>
+      <h3>제 키는 {height}</h3>
+      <a>나이는 {age > 30 ? "30대" : "20대"}</a>
+      <div>저는 {major}을 전공하고 있고 잘부탁 드립니다!</div>
     </div>
   );
 }
